ad2cd72d8f1dccde6e7c60533fd6941064de41e6
Son Le <son.le1@tyme.com>
2026-08-13 11:13:01 +0700

    build: enable lifecycle flow and unit tests

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -33,11 +33,16 @@ dependencies {
     implementation(libs.androidx.compose.ui)
     implementation(libs.androidx.compose.material3)
     implementation(libs.androidx.compose.ui.tooling.preview)
+    implementation(libs.androidx.lifecycle.runtime.compose)
+    implementation(libs.androidx.lifecycle.viewmodel.compose)
     implementation(libs.androidx.navigation.compose)
+    implementation(libs.kotlinx.coroutines.core)
 
     debugImplementation(libs.androidx.compose.ui.tooling)
     debugImplementation(libs.androidx.compose.ui.test.manifest)
 
     androidTestImplementation(platform(libs.androidx.compose.bom))
     androidTestImplementation(libs.androidx.compose.ui.test.junit4)
+
+    testImplementation(libs.junit4)
 }
